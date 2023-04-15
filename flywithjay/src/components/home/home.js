cfcafdb186eb701b2cef413c4a7e4efa736cc67d
NIKUNJ DAVE <dnikunj178@gmail.com>
2023-04-15 15:54:38 +0530

    'm'

---
+++ flywithjay/src/components/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../header/header';
+import About from '../about/about';
+import Footer from '../footer/footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>FLY WITH JAY!</h1>
+    </div>
+  );
+};
+
+export default Home;
